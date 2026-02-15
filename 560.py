1ce0c2cf737353fd2c041881e2680f8de8ceb8f7
Krishna Singh <samar.phone.home@gmail.com>
2026-02-16 02:26:59 +0530

    leetcode 560

---
+++ 560.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0 
+        cur_sum = 0
+        prefix_sum = {0:1}
+
+        for num in nums:
+            cur_sum+=num
+
+            diff = cur_sum-k
+            if diff in prefix_sum:
+                count+=prefix_sum[diff]
+
+            prefix_sum[cur_sum]= prefix_sum.get(cur_sum, 0)+1
+
+        return count
