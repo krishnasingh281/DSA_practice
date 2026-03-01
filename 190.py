03b842def663aeb281678556e8bd1a99591d6bda
Krishna Singh <samar.phone.home@gmail.com>
2026-03-01 23:58:05 +0530

    LEETCODE 190

--- 190.py
+++ 190.py
@@ -3,6 +3,5 @@ class Solution:
 
         binary = f"{n:032b}"
         reversed_binary = binary[::-1]
-    
-        # 3. Convert that reversed string back to an integer (Base 2)
+
         return int(reversed_binary, 2)
