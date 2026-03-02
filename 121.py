b1348d6ea749358680419ea55925f419e4a628e1
Krishna Singh <samar.phone.home@gmail.com>
2026-03-02 23:46:22 +0530

    LEETCODE 121

--- 121.py
+++ 121.py
@@ -4,7 +4,7 @@ class Solution:
         max_profit = 0      # Track the best profit found so far
 
         for price in prices:
-            # 1. Update the minimum price seen so far
+            
             if price < mini:
                 mini = price
             
