cb4ddec4935fdc473a078669c1c9e44799200715
Krishna Singh <samar.phone.home@gmail.com>
2026-03-05 00:00:55 +0530

    LEETCODE 704

--- 704.py
+++ 704.py
@@ -2,7 +2,6 @@ class Solution:
     def search(self, nums: list[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
-
         while left <= right:
             mid = left + (right - left) // 2
 
