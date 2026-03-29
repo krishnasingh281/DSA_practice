1fedac8de9170f576d5ad167bbac7f243de260d6
Krishna Singh <samar.phone.home@gmail.com>
2026-03-29 23:59:38 +0530

    leetcode 1781

--- 1781.py
+++ 1781.py
@@ -15,3 +15,31 @@ class Solution:
                     sumu += max(counts) - min(counts)
                 
         return sumu
+    
+
+
+    class Solution:
+    def beautySum(self, s: str) -> int:
+        def diff(substring):
+            freq_map = {}
+            
+            for i in range(len(substring)):
+                char = substring[i]
+                if char in freq_map:
+                    freq_map[char] += 1
+                else:
+                    freq_map[char] = 1
+
+            return (max(freq_map.values()) - min(freq_map.values()))
+
+        list1 = []
+
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                substring = s[i : j + 1]
+                list1.append(diff(substring))
+
+        
+        return sum(list1)
+            
+         
