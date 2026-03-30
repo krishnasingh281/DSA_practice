7dc739b500547655ba2be001510a19514c137842
Krishna Singh <samar.phone.home@gmail.com>
2026-03-30 23:47:43 +0530

    leetcode 38

---
+++ 38.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = '1'
+        if n == 1:
+            return s
+            
+        for l in range(n - 1):
+            next_s = []
+            i = 0
+            while i<len(s):
+                count=1
+                while i+1<len(s) and s[i] == s[i+1]:
+                    count+=1
+                    i+=1
+                    
+                next_s.append(str(count))
+                next_s.append(s[i])
+                i+=1
+            
+            s = "".join(next_s)
+            
+        return s
