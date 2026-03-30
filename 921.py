be2f48c181b74d8861167b204ba3298b9d13d4a9
Krishna Singh <samar.phone.home@gmail.com>
2026-03-30 23:46:37 +0530

    leetcode 921

---
+++ 921.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+
+        dial1 = 0
+        dial2 = 0
+
+        for char in s:
+            if char == '(':
+                dial1 += 1
+
+            elif char == ")" and dial1 != 0:
+                dial1 -= 1
+
+            else:
+                dial2 += 1
+
+        return dial1 + dial2
+        
