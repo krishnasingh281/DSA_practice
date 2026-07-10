1b5a059b5809370da55612d5e3609927d64bc040
Krishna Singh <samar.phone.home@gmail.com>
2026-07-11 03:04:48 +0530

    Time: 0 ms (100%), Space: 20.5 MB (17.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,22 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        indexed_nums = []
-        for idx in range(len(nums)):
-            indexed_nums.append([nums[idx], idx])
-        
-
-        indexed_nums.sort()
-        
-        i = 0
-        j = len(nums) - 1
-
-        while i < j:
-            current_sum = indexed_nums[i][0] + indexed_nums[j][0]
-            
-            if current_sum == target:
-                return [indexed_nums[i][1], indexed_nums[j][1]]
-            elif current_sum > target:
-                j -= 1
-            else:
-                i += 1
-        return []
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap={}
+        for i in range(len(nums)):
+            y=target-nums[i]
+            if y in hashmap:
+                return [i,hashmap[y]]
+            hashmap[nums[i]]=i
